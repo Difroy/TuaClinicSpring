b60c0f701e630a2c6f371dc7e8953ae08913c07a
Difroy <unifroy@gmail.com>
2024-09-16 15:00:15 +0200

    Update PatientAPI

--- src/main/java/com/generation/tuaclinicspring/api/PatientAPI.java
+++ src/main/java/com/generation/tuaclinicspring/api/PatientAPI.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.tuaclinicspring.model.entities.Patient;
@@ -18,12 +20,23 @@ public class PatientAPI {
 		PatientRepository repo;
 		
 		
-		
+		// R in CRUD
 		@GetMapping("/patients")
 		public List<Patient>getPatients(){
-			
 			return repo.findAll();
 		}
+		
+		@PostMapping("/patients")
+		public Patient newPatient (@RequestBody Patient newPatient) {
+			
+			
+			return repo.save(newPatient);
+		}
+		
+		
+		
+		
+		
 		
 		
 		
