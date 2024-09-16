20f36ed3824a30d9f8fbdaeca273215a2324410c
Difroy <unifroy@gmail.com>
2024-09-16 14:32:29 +0200

    Create PatientRepository.java

---
+++ src/main/java/com/generation/tuaclinicspring/model/repository/PatientRepository.java
@@ -0,0 +1,14 @@
+package com.generation.tuaclinicspring.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.tuaclinicspring.model.entities.Patient;
+
+public interface PatientRepository extends JpaRepository <Patient, Long> {
+
+	
+	
+	
+	
+	
+}
