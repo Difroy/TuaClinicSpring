75218df8381dca6c84307d48da1db3943bb8572d
Difroy <unifroy@gmail.com>
2024-09-17 14:10:32 +0200

    Update

---
+++ src/main/java/com/generation/tuaclinicspring/model/dto/DoctorDTO.java
@@ -0,0 +1,5 @@
+package com.generation.tuaclinicspring.model.dto;
+
+public class DoctorDTO {
+
+}
--- src/main/java/com/generation/tuaclinicspring/model/entities/Vaccination.java
+++ src/main/java/com/generation/tuaclinicspring/model/entities/Vaccination.java
@@ -1,5 +1,61 @@
 package com.generation.tuaclinicspring.model.entities;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Vaccination {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	LocalDate date;
+	String vaccine;
+	
+	@ManyToOne
+	Doctor doctor;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getVaccine() {
+		return vaccine;
+	}
+
+	public void setVaccine(String vaccine) {
+		this.vaccine = vaccine;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	
+	
+	
+	
+	
 }
