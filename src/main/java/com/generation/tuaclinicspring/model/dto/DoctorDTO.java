8367e3b6ea6def3008c249fea1825bf176266f14
Difroy <unifroy@gmail.com>
2024-09-17 14:21:38 +0200

    Update DoctorDTO.java

--- src/main/java/com/generation/tuaclinicspring/model/dto/DoctorDTO.java
+++ src/main/java/com/generation/tuaclinicspring/model/dto/DoctorDTO.java
@@ -1,6 +1,10 @@
 package com.generation.tuaclinicspring.model.dto;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.generation.tuaclinicspring.model.entities.Gender;
+import com.generation.tuaclinicspring.model.entities.Specialization;
 
 public class DoctorDTO {
 
@@ -11,6 +15,70 @@ public class DoctorDTO {
 	Gender gender;
 	Specialization specialization;
 	List<VaccinationDTO> vaccination = new ArrayList<VaccinationDTO>();
+	
+	
+	public DoctorDTO() {}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public Gender getGender() {
+		return gender;
+	}
+
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+
+	public Specialization getSpecialization() {
+		return specialization;
+	}
+
+
+	public void setSpecialization(Specialization specialization) {
+		this.specialization = specialization;
+	}
+
+
+	public List<VaccinationDTO> getVaccination() {
+		return vaccination;
+	}
+
+
+	public void setVaccination(List<VaccinationDTO> vaccination) {
+		this.vaccination = vaccination;
+	}
+	
 	
 	
 	
